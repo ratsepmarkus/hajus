17917b162a7600202164bea795783e8a2eecef70
ratsepmarkus <markusratsep11@gmail.com>
2022-06-04 22:08:01 +0300

    try999999

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,26 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
-    public function index()
+    public function Weather()
     {
-        $lat = 58.25;
-        $lon = 22.49;
-
+        $apiKey = env('API_KEY');
+        $lat = 58.254951;
+        $lon = 22.491898;
         if (Cache::has("weather")) {
             $data = Cache::get('weather');
         } else {
-            $response = Http::get("https://api.openweathermap.org/data/2.5/weather?lat=%7B$lat%7D&lon=%7B$lon%7D&units=metric&appid=" . config("services.weather.key"));
-            // return $response;
-            Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]));
+            $response = Http::get("https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&units=metric&appid=" . config("services.weather.key"));
+            Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]), 150);
             $data = Cache::get("weather");
         }
-        return Inertia::render('weather', [
+        return Inertia::render("Weather", [
             'data' => $data
         ]);
     }
--- resources/js/Pages/Weather.vue
+++ resources/js/Pages/Weather.vue
@@ -1,20 +1,17 @@
 <template>
+
   <Head title="Weather" />
-    <div class="max-w-7xl mx-auto px-6 lg:px-8 py-12">
-      <div class="bg-white overflow-hidden shadow-sm rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200">
-          <div class="text-2xl">Koht: {{ data.name }} </div>
-          <div class="text-2xl">Ilm: {{ data.weather[0].main }}</div>
-          <div class="text-2xl">Temperatuur: {{ data.main.temp }}</div>
-          <div class="text-2xl">Tundub nagu: {{data.main.feels_like}}</div>
-          <div class="text-2xl">Tuule kiirus: {{data.wind.speed}}</div>
-          <div class="text-2xl"></div>
-          <div class="text-2xl"></div>
-          <div class="text-2xl"></div>
-          <div class="text-2xl"></div>
-        </div>
+  <div class="max-w-7xl mx-auto px-6 lg:px-8 py-12">
+    <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+      <div class="p-6 bg-white border-b border-gray-200">
+        <div class="text-2xl">Koht: {{ data.name }} </div>
+        <div class="text-2xl">Ilm: {{ data.weather[0].main }}</div>
+        <div class="text-2xl">Temperatuur: {{ data.main.temp }}</div>
+        <div class="text-2xl">Tundub nagu: {{data.main.feels_like}}</div>
+        <div class="text-2xl">Tuule kiirus: {{data.wind.speed}}</div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
