4169a2b09465a78755163f3870bdcae8f44a737a
ratsepmarkus <markusratsep11@gmail.com>
2022-06-04 21:41:43 +0300

    yes

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,25 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
-    public function Weather()
+    public function index()
     {
-        $apiKey = env('API_KEY');
-        $lat = 58.254951;
-        $lon = 22.491898;
+        $lat = 58.25;
+        $lon = 22.49;
+
         if (Cache::has("weather")) {
             $data = Cache::get('weather');
         } else {
             $response = Http::get("https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&units=metric&appid=" . config("services.weather.key"));
-            Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]), 150);
+            // return $response;
+            Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]));
             $data = Cache::get("weather");
         }
-        return Inertia::render("Weather", [
+        return Inertia::render('Weather', [
             'data' => $data
         ]);
     }
